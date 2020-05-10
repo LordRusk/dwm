3a9b1f1d25504a8e0c76f0ad00d67a9148b3d764
rusk <lordruskk@gmail.com>
2020-05-10 22:40:26 +0000

    changed from personal font to monospaced 10pt by default

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
-	"Ibm Plex Mono:size=8",
-	"JoyPixels:size=8",
+	"monospace:size=10",
+	"JoyPixels:size=10",
 };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
