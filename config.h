5a9da2feb23f285e71cd47aabdc20e835eb74a1a
rusk <lordruskk@gmail.com>
2020-06-13 10:38:40 -0700

    bmon now starts observing wifi card

--- config.h
+++ config.h
@@ -168,7 +168,7 @@ static Key keys[] = {
 
 	/* spawn Keys */
 	{ MODKEY|ShiftMask,             XK_Return,	spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("$TERMINAL -e bmon") },
+	{ MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("$TERMINAL -e bmon -p wlp*,*") },
 	{ MODKEY,			XK_d,		spawn,		SHCMD("dmenu_run -l 20") },
 	{ MODKEY,			XK_v,		spawn,		SHCMD("$TERMINAL -e lf") },
 	{ MODKEY,			XK_e,		spawn,		SHCMD("$TERMINAL -e neomutt; kill -$((34+12)) $(pidof dwmblocks)") },
