55775743e1e89321a36ab39fa785bc3d58585160
lordrusk <lordruskk@gmail.com>
2020-06-25 16:23:58 +0000

    use font awesome as another default

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int swallowfloating    = 1;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:size=10", "Font Awesome:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
