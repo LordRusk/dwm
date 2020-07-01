1653e6e6768ae1161cc44bbedcca3f23ba7493f8
lordrusk <lordruskk@gmail.com>
2020-07-01 18:45:54 +0000

    added roman numerals optionally

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static Sp scratchpads[] = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):
